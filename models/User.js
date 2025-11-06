aa74378eb58263e36b5ad86bed088ab5355dd226
mukky254 <muhidinyusuf06@gmail.com>
2025-11-06 22:41:40 +0300

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    lowercase: true
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please add a phone number']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6
+  },
+  role: {
+    type: String,
+    enum: ['farmer', 'wholesaler', 'retailer', 'admin'],
+    default: 'farmer'
+  },
+  profile: {
+    location: {
+      county: String,
+      subCounty: String,
+      ward: String
+    },
+    farmDetails: {
+      type: String,
+      default: ''
+    },
+    businessName: {
+      type: String,
+      default: ''
+    },
+    businessType: {
+      type: String,
+      default: ''
+    }
+  },
+  avatar: {
+    type: String,
+    default: ''
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  totalReviews: {
+    type: Number,
+    default: 0
+  }
+}, {
+  timestamps: true
+});
+
+// Encrypt password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Match password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
